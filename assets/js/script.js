0fe4a98cce7ff328fe436af1cee300adb4650dfb
michael-hoang <mike081522@gmail.com>
2023-03-25 10:12:47 -0700

    Link js to html, create first div square

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const squareDivsContainer = document.querySelector('#square-divs-container');
+const newDiv = document.createElement('div');
+newDiv.style.border = 'thin solid black';
+newDiv.style.height = '25px';
+newDiv.style.width = '25px';
+squareDivsContainer.appendChild(newDiv);
