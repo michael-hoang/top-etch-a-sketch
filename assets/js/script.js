fc608836992f26f61894221979adbd5cf4d6496d
michael-hoang <mike081522@gmail.com>
2023-03-25 14:25:14 -0700

    Create drawGrid function

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,34 +15,35 @@ squareDivsContainer.style.width = `${containerWidth}px`;
 squareDivsContainer.style.height = `${containerWidth}px`;
 squareDivsContainer.style.fontSize = 0;
 
-let x, y;
-x = y = squaresPerSide;
-// Draw grid
-for (let i = 0; i < y; i++) {
-    for (let j = 0; j < x; j++) {
-        const newDiv = document.createElement('div');
-        newDiv.style.border = `solid black`;
-        newDiv.style.height = `${squareSize}px`;
-        newDiv.style.width = `${squareSize}px`;
-        newDiv.style.display = 'inline-block';
-        squareDivsContainer.appendChild(newDiv);
-
-        // Squares that are not located at right edge, bottom edge, or bottom right corner
-        if (i !== y - 1 && j !== x - 1) {
-            newDiv.style.borderWidth = `${borderThickness}px 0 0 ${borderThickness}px`;
-        } // Right edge squares (exclude bottom right corner)
-        else if (i !== y - 1 && j === x - 1) {
-            newDiv.style.borderWidth = `${borderThickness}px ${borderThickness}px 0 ${borderThickness}px`;
-        } // Bottom edge squares (exclude bottom right corner)
-        else if (i === y - 1 && j !== x - 1) {
-            newDiv.style.borderWidth = `${borderThickness}px 0 ${borderThickness}px ${borderThickness}px`;
-        } // Bottom right corner square
-        else {
-            newDiv.style.borderWidth = `${borderThickness}px`;
+function drawGrid() {
+    let x, y;
+    x = y = squaresPerSide;
+    for (let i = 0; i < y; i++) {
+        for (let j = 0; j < x; j++) {
+            const newDiv = document.createElement('div');
+            newDiv.style.border = `solid black`;
+            newDiv.style.height = `${squareSize}px`;
+            newDiv.style.width = `${squareSize}px`;
+            newDiv.style.display = 'inline-block';
+            squareDivsContainer.appendChild(newDiv);
+    
+            // Squares that are not located at right edge, bottom edge, or bottom right corner
+            if (i !== y - 1 && j !== x - 1) {
+                newDiv.style.borderWidth = `${borderThickness}px 0 0 ${borderThickness}px`;
+            } // Right edge squares (exclude bottom right corner)
+            else if (i !== y - 1 && j === x - 1) {
+                newDiv.style.borderWidth = `${borderThickness}px ${borderThickness}px 0 ${borderThickness}px`;
+            } // Bottom edge squares (exclude bottom right corner)
+            else if (i === y - 1 && j !== x - 1) {
+                newDiv.style.borderWidth = `${borderThickness}px 0 ${borderThickness}px ${borderThickness}px`;
+            } // Bottom right corner square
+            else {
+                newDiv.style.borderWidth = `${borderThickness}px`;
+            }
+    
+            // Add "hover" effect to square
+            newDiv.addEventListener('mouseover', (e) => newDiv.style.backgroundColor = `${squareColor}`);
         }
-
-        // Add "hover" effect to square
-        newDiv.addEventListener('mouseover', (e) => newDiv.style.backgroundColor = `${squareColor}`);
     }
 }
 
