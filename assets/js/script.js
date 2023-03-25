72202119c07c32b4793dd239714bbe0737d559e6
michael-hoang <mike081522@gmail.com>
2023-03-25 11:45:05 -0700

    Create grid of squares using loops

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,36 @@
+// Grid dimension
+let x = 16;
+let y = 16;
+let squareHeight = 25;
+let squareWidth = 25;
+let borderThickness = 1;
+
+
 const squareDivsContainer = document.querySelector('#square-divs-container');
-const newDiv = document.createElement('div');
-newDiv.style.border = 'thin solid black';
-newDiv.style.height = '25px';
-newDiv.style.width = '25px';
-squareDivsContainer.appendChild(newDiv);
+squareDivsContainer.style.height = `${(squareHeight + borderThickness) * y + borderThickness}px`;
+squareDivsContainer.style.width = `${(squareWidth + borderThickness) * y + borderThickness}px`;
+squareDivsContainer.style.fontSize = 0;
+
+for (let i = 0; i < y; i++) {
+    for (let j = 0; j < x; j++) {
+        const newDiv = document.createElement('div');
+        newDiv.style.border = `solid black`;
+        // Set initial border thickness of squares
+        newDiv.style.borderWidth = `${borderThickness}px 0 0 ${borderThickness}px`;
+        newDiv.style.height = `${squareHeight}px`;
+        newDiv.style.width = `${squareWidth}px`;
+        newDiv.style.display = 'inline-block';
+        squareDivsContainer.appendChild(newDiv);
+
+        // Right edge squares (exclude bottom right corner)
+        if (i !== y - 1 && j === x - 1) {
+            newDiv.style.borderWidth = `${borderThickness}px ${borderThickness}px 0 ${borderThickness}px`;
+        } // Bottom edge squares (exclude bottom right corner)
+        else if (i === y - 1 && j !== x - 1) {
+            newDiv.style.borderWidth = `${borderThickness}px 0 ${borderThickness}px ${borderThickness}px`;
+        } // Bottom right corner square
+        else if (i === y - 1 && j === x - 1) {
+            newDiv.style.borderWidth = `${borderThickness}px`
+        }
+    }
+}
