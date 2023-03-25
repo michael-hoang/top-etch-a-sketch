03262de49e74b3d328465bb50ba101926fafdfd3
michael-hoang <mike081522@gmail.com>
2023-03-25 11:55:49 -0700

    Refactor grid looping algorithm

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,21 +15,22 @@ for (let i = 0; i < y; i++) {
     for (let j = 0; j < x; j++) {
         const newDiv = document.createElement('div');
         newDiv.style.border = `solid black`;
-        // Set initial border thickness of squares
-        newDiv.style.borderWidth = `${borderThickness}px 0 0 ${borderThickness}px`;
         newDiv.style.height = `${squareHeight}px`;
         newDiv.style.width = `${squareWidth}px`;
         newDiv.style.display = 'inline-block';
         squareDivsContainer.appendChild(newDiv);
 
-        // Right edge squares (exclude bottom right corner)
-        if (i !== y - 1 && j === x - 1) {
+        // Squares that are not located at right edge, bottom edge, or bottom right corner
+        if (i !== y - 1 && j !== x - 1) {
+            newDiv.style.borderWidth = `${borderThickness}px 0 0 ${borderThickness}px`;
+        } // Right edge squares (exclude bottom right corner)
+        else if (i !== y - 1 && j === x - 1) {
             newDiv.style.borderWidth = `${borderThickness}px ${borderThickness}px 0 ${borderThickness}px`;
         } // Bottom edge squares (exclude bottom right corner)
         else if (i === y - 1 && j !== x - 1) {
             newDiv.style.borderWidth = `${borderThickness}px 0 ${borderThickness}px ${borderThickness}px`;
         } // Bottom right corner square
-        else if (i === y - 1 && j === x - 1) {
+        else {
             newDiv.style.borderWidth = `${borderThickness}px`
         }
     }
